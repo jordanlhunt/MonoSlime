6a6495b2e1cff3beed19674184aacf42adfc6cad
Jordan Hunt <jordan.hunt@g.austincc.edu>
2025-09-04 14:44:43 -0500

    modified Game.cs to use Core library

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -1,34 +1,25 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
 
 namespace DungeonSlime;
 
-public class Game1 : Game
+public class Game1 : Core
 {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
-
+    #region Constants
+    const int DEFAULT_WINDOW_WIDTH = 1280;
+    const int DEFAULT_WINDOW_HEIGHT = 720;
+    #endregion
     public Game1()
-    {
-        _graphics = new GraphicsDeviceManager(this);
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
-    }
+        : base("Dungeon Slime", DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, false) { }
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
-
         base.Initialize();
     }
 
-    protected override void LoadContent()
-    {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        // TODO: use this.Content to load your game content here
-    }
+    protected override void LoadContent() { }
 
     protected override void Update(GameTime gameTime)
     {
@@ -37,18 +28,12 @@ public class Game1 : Game
             || Keyboard.GetState().IsKeyDown(Keys.Escape)
         )
             Exit();
-
-        // TODO: Add your update logic here
-
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.MonoGameOrange);
-
-        // TODO: Add your drawing code here
-
         base.Draw(gameTime);
     }
 }
--- DungeonSlime/obj/Debug/net8.0/DungeonSlime.AssemblyInfo.cs
+++ DungeonSlime/obj/Debug/net8.0/DungeonSlime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9e7bbcb03c70de0bf10cd14ff2660262c6a2425")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f0e25bda1db75d12293080a8306ba1099225e70")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
