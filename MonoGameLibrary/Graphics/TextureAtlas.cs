230d32971a2d15c6fd93f25aadbdd667e7f338fd
Jordan Hunt <jordan.hunt@g.austincc.edu>
2025-09-06 10:19:05 -0500

    Added TextureAtlas.cs

--- MonoGameLibrary/Graphics/TextureAtlas.cs
+++ MonoGameLibrary/Graphics/TextureAtlas.cs
@@ -4,3 +4,17 @@ using System.Xml;
 using System.Xml.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGameLibrary.Graphics;
+
+public class TextureAtlas
+{
+    #region Member Variables
+    private Dictionary<string, TextureRegion> textureRegionsDictionary;
+    #endregion
+
+    #region Properties
+    public Texture2D Texture { get; set; }
+    #endregion
+}
