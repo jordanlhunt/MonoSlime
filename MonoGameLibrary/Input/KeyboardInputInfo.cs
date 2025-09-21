18d51dc7030a216c132a28bf215ccf0476680e0e
Jordan Hunt <jordan.hunt@g.austincc.edu>
2025-09-20 21:46:47 -0500

    Started support for InputHandler in Chapter 11

---
+++ MonoGameLibrary/Input/KeyboardInputInfo.cs
@@ -0,0 +1,41 @@
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameLibrary.Input;
+
+public class KeyboardInputInfo
+{
+    #region Properties
+    public KeyboardState PreviousState { get; private set; }
+    public KeyboardState CurrentState { get; private set; }
+    #endregion
+
+    #region Constructor
+    public KeyboardInputInfo()
+    {
+        PreviousState = new KeyboardState();
+        CurrentState = PreviousState;
+    }
+    #endregion
+    #region Public Methods
+    public void Update()
+    {
+        PreviousState = CurrentState;
+        CurrentState = Keyboard.GetState();
+    }
+
+    public bool IsKeyDown(Keys key)
+    {
+        return CurrentState.IsKeyDown(key);
+    }
+
+    public bool WasKeyJustPressed(Keys key)
+    {
+        return CurrentState.IsKeyDown(key) && PreviousState.IsKeyUp(key);
+    }
+
+    public bool WasKeyJustReleased(Keys key)
+    {
+        return CurrentState.IsKeyUp(key) && PreviousState.IsKeyDown(key);
+    }
+    #endregion
+}
