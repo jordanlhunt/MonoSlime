8b23323970e7c801582c86f889f2cc66fc769edb
Jordan Hunt <jordan.hunt@g.austincc.edu>
2025-10-12 09:28:34 -0500

    I can't focus today for some reason

--- MonoGameLibrary/Graphics/Tileset/TileSet.cs
+++ MonoGameLibrary/Graphics/Tileset/TileSet.cs
@@ -2,12 +2,38 @@
 {
     public class TileSet
     {
+        #region Member Variables
+        private readonly TextureRegion[] tiles;
+        #endregion
         #region Properties
         public int TileWidth { get; }
         public int TileHeight { get; }
         public int Columns { get; }
         public int Rows { get; }
         public int Count { get; }
+        #endregion
+        #region Constructor
+        public TileSet(TextureRegion textureRegion, int tileWidth, int tileHeight)
+        {
+            this.TileWidth = tileWidth;
+            this.TileHeight = tileHeight;
+            Columns = textureRegion.Width / tileWidth;
+            Rows = textureRegion.Height / tileHeight;
+            Count = Columns * Rows;
+            tiles = new TextureRegion[Count];
+            for (int i = 0; i < Count; i++)
+            {
+                int x = i % Columns * tileWidth;
+                int y = i / Columns * tileHeight;
+                tiles[i] = new TextureRegion(
+                    textureRegion.Texture,
+                    textureRegion.SourceRectangle.X + x,
+                    textureRegion.SourceRectangle.Y + y,
+                    tileWidth,
+                    tileHeight
+                );
+            }
+        }
         #endregion
     }
 }
