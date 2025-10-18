1ee7a5141c2d959524c7d6f207088d6c6be22dca
Jordan Hunt <jordan.hunt@g.austincc.edu>
2025-10-18 09:52:16 -0500

    Did a little bit of the Tilemap before heading to class. I'm going to finish chapter 13 tonight no matter what!

--- MonoGameLibrary/Graphics/Tiles/TileMap.cs
+++ MonoGameLibrary/Graphics/Tiles/TileMap.cs
@@ -1,3 +1,5 @@
+using Microsoft.Xna.Framework;
+
 namespace MonoGameLibrary.Graphics.Tiles;
 
 public class Tilemap
@@ -5,5 +7,27 @@ public class Tilemap
     #region Member Variables
     private readonly TileSet tileSet;
     private readonly int[] tileIds;
+    #endregion
+    #region Properties
+    public int Rows { get; }
+    public int Columns { get; }
+    public int Count { get; }
+    public Vector2 Scale { get; set; }
+    public float TileWidth
+    {
+        get { return tileSet.TileWidth * Scale.X; }
+    }
+    public float TileHeight
+    {
+        get { return tileSet.TileHeight * Scale.Y; }
+    }
+    #endregion
+    #region Constructor
+    public Tilemap(TileSet tileSet, int columns, int rows)
+    {
+        this.tileSet = tileSet;
+        Rows = rows;
+        Columns = columns;
+    }
     #endregion
 }
