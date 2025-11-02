805167179e61f47097d13c254d4ff9a9d26491d1
Jordan Hunt <jordan.hunt@g.austincc.edu>
2025-11-02 10:08:43 -0600

    Revert "Completed chapater 15"
    This reverts commit f9023810e24355f4a4f84dcfc2a4cbd6266a620a.

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -22,28 +22,21 @@ public class Game1 : Core
     private const string ThemeSongLocation = "Sounds/theme";
     private const string AtlasDefinitionLocation = "Images/atlas-definition.xml";
     private const string TilemapDefinitionLocation = "Images/tilemap-definition.xml";
-    private const string EquipmentProSpriteFontLocation = "Fonts/Equipmentpro";
     private const float MovementSpeed = 5.0f;
-    private const string Score = "Score";
     #endregion
     #region Member Variables
     private AnimatedSprite slime;
-    private AnimatedSprite bat;
     private Vector2 slimePosition;
     private Vector2 batPosition;
     private Vector2 batVelocity;
-    private Vector2 scoreTextPosition;
-    private Vector2 scoreTextOrigin;
-    private Rectangle roomBounds;
+    private AnimatedSprite bat;
     private Rectangle screenBounds;
     private Circle slimeBoundsCircle;
     private Circle batBoundsCircle;
     private Tilemap tileMap;
+    private Rectangle roomBounds;
     private SoundEffect bounceSoundEffect;
     private SoundEffect collectSoundEffect;
-    private int currentScore;
-    private SpriteFont spriteFont;
-
     private Song themeSong;
     #endregion
     #region Constructor
@@ -77,9 +70,6 @@ public class Game1 : Core
             $"Expected size: {tileMap.Columns * tileMap.TileWidth}x{tileMap.Rows * tileMap.TileHeight}"
         );
         Audio.PlaySong(themeSong);
-        scoreTextPosition = new Vector2(roomBounds.Left, tileMap.TileHeight * .5f);
-        float scoreTextYOrigin = spriteFont.MeasureString(Score).Y * 0.5f;
-        scoreTextOrigin = new Vector2(0, scoreTextYOrigin);
     }
 
     protected override void LoadContent()
@@ -95,7 +85,6 @@ public class Game1 : Core
         bounceSoundEffect = Content.Load<SoundEffect>(BounceSoundEffectLocation);
         collectSoundEffect = Content.Load<SoundEffect>(CollectSoundEffectLocation);
         themeSong = Content.Load<Song>(ThemeSongLocation);
-        spriteFont = Content.Load<SpriteFont>(EquipmentProSpriteFontLocation);
     }
 
     protected override void Update(GameTime gameTime)
@@ -119,19 +108,7 @@ public class Game1 : Core
         tileMap.Draw(SpriteBatch);
         slime.Draw(SpriteBatch, slimePosition);
         bat.Draw(SpriteBatch, batPosition);
-        SpriteBatch.DrawString(
-            spriteFont,
-            Score + ": " + currentScore,
-            scoreTextPosition,
-            Color.White,
-            0.0f,
-            scoreTextOrigin,
-            1.0f,
-            SpriteEffects.None,
-            0.0f
-        );
         SpriteBatch.End();
-
         base.Draw(gameTime);
     }
     #endregion
@@ -286,7 +263,6 @@ public class Game1 : Core
             batPosition = new Vector2(column * bat.Width, row * bat.Height);
             AssignRandomBatVelocity();
             Audio.PlaySoundEffect(collectSoundEffect);
-            currentScore += 100;
         }
     }
 
--- DungeonSlime/obj/Debug/net8.0/DungeonSlime.AssemblyInfo.cs
+++ DungeonSlime/obj/Debug/net8.0/DungeonSlime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7eb7651497aadd3a5c91a99b72448de71496176")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c82b845940663db8ed62b6e00fa9af36b3b79e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
