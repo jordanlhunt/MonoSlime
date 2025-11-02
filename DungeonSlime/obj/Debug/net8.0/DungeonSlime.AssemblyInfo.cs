a4ee513772037894b0a0dcac28de345d5f2ce07f
Jordan Hunt <jordan.hunt@g.austincc.edu>
2025-11-02 10:09:19 -0600

    Completed chapater 16

--- DungeonSlime/obj/Debug/net8.0/DungeonSlime.AssemblyInfo.cs
+++ DungeonSlime/obj/Debug/net8.0/DungeonSlime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c82b845940663db8ed62b6e00fa9af36b3b79e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+805167179e61f47097d13c254d4ff9a9d26491d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
